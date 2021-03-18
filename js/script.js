af7924692f070a1b375dc6e3b640cdf6acc712db
Michele Lorusso <kirito102095@hotmail.it>
2021-03-18 15:23:24 +0100

    Consegna Esercizio

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-var nomeUtente = prompt("Ciao, dimmi il tuo nome?")
-console.log(nomeUtente);
+var nomeUtente = prompt("Ciao, Qual'è il tuo nome?")
+// console.log(nomeUtente);
+
+var cognomeUtente = prompt("Ciao " + nomeUtente + ", Mi diresti anche il tuo Cognome?");
+// console.log(cognomeUtente);
+
+var colorPrefer = prompt(nomeUtente + " " + cognomeUtente + ", Qual'è il tuo colore preferito?")
+// console.log(colorPrefer);
+
+var password = nomeUtente + cognomeUtente + colorPrefer + "21";
+
+document.getElementById('pass').innerHTML = password;
